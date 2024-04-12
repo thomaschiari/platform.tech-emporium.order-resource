06048307bf2813fbcb2c6d43db474a7013b5484e
Lucca Hiratsuca Costa <luccahiratsuca@gmail.com>
2024-04-12 10:55:45 -0300

    feat: swagger fix: correct name for sql table

--- src/main/java/com/insper/store/order/OrderModel.java
+++ src/main/java/com/insper/store/order/OrderModel.java
@@ -15,7 +15,7 @@ import lombok.Setter;
 import lombok.experimental.Accessors;
 
 @Entity
-@Table(name = "order")
+@Table(name = "orders")
 @EqualsAndHashCode(of = "id")
 @Builder @Getter @Setter @Accessors(chain = true, fluent = true)
 @NoArgsConstructor @AllArgsConstructor
